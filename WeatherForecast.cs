10c73d838417120994fb3a44858eb3a8e3d85569
RJK416 <gegaoragvelidze97@gmail.com>
2024-12-30 20:18:35 +0400

    using git command instead of Visual Studio

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace GitTestingAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary2 { get; set; }
+
+        public int iii { get; set; }
     }
 }
